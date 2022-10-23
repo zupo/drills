74a2069f5207897980c97185d447694dc213e21d
Nejc Zupan <nejc.zupan@gmail.com>
2022-10-23 14:42:34 +0200

    base nix shell

---
+++ shell.nix
@@ -0,0 +1,16 @@
+let
+  nixpkgs = builtins.fetchTarball {
+    # https://status.nixos.org/ -> nixos-22.05 on 2022-10-23
+    url = "https://github.com/nixos/nixpkgs/archive/3933d8bb9120573c0d8d49dc5e890cb211681490.tar.gz";
+  };
+  pkgs = import nixpkgs {};
+
+in
+
+pkgs.mkShell {
+  name = "dev-shell";
+  buildInputs = [
+
+  ];
+
+}
