97b756f56c93bef757f34f3fa2ae84cb2499bc6f
Nejc Zupan <nejc.zupan@gmail.com>
2022-10-23 19:33:01 +0200

    initial game version, single word, no styling

--- src/Main.elm
+++ src/Main.elm
@@ -3,6 +3,71 @@ module Main exposing (..)
 import Html exposing (p, text)
 import Html.Attributes exposing (class)
 
+import Browser exposing (Document)
+import Html exposing (Html, Attribute, div, input, text)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+
+--main =
+--  p [ class "text-sm" ] [ text "Hello!"]
 
 main =
-  p [ class "text-sm" ] [ text "Hello!"]
+  Browser.document { init = init, update = update, subscriptions = subscriptions, view = view }
+
+-- MODEL
+
+type alias Model =
+  { actual : String,
+    expected : String
+  }
+
+
+init : () -> (Model, Cmd Msg)
+init _ =
+  ({ actual = "", expected = "hello" }, Cmd.none)
+
+
+-- UPDATE
+
+
+type Msg
+  = Change String
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Change newContent ->
+      ({ model | actual = newContent }, Cmd.none)
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
+-- VIEW
+
+
+view : Model -> Document Msg
+view model =
+  {
+    title = "Spellings",
+    body = [
+  div []
+    [ input [ placeholder "Start typing!", value model.actual, onInput Change, attribute "autofocus" ""] []
+    , viewValidation model ]
+    ]
+  }
+
+
+
+viewValidation : Model -> Html msg
+viewValidation model =
+  if model.actual == model.expected then
+    div [ style "color" "green" ] [ text "OK" ]
+  else
+    div [ style "color" "red" ] [ text "Keep trying!" ]
