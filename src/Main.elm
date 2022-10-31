76a0e2cdb2b73218556ff278c6a73866b0986060
Nejc Zupan <nejc.zupan@gmail.com>
2022-10-31 21:45:52 +0000

    support for pronounciation

--- src/Main.elm
+++ src/Main.elm
@@ -1,53 +1,67 @@
-module Main exposing (..)
-
-import Html exposing (p, text)
-import Html.Attributes exposing (class)
+port module Main exposing (..)
 
 import Array
-
 import Browser exposing (Document)
-import Html exposing (Html, Attribute, button, div, input, text)
-import Html.Events exposing (onClick)
+import Html exposing (Attribute, Html, button, div, input, p, text)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
+import Html.Events exposing (onClick, onInput)
+
 
 main =
-  Browser.document { init = init, update = update, subscriptions = subscriptions, view = view }
+    Browser.document { init = init, update = update, subscriptions = subscriptions, view = view }
+
+
 
 -- MODEL
 
+
 type alias Model =
-  { actual : String,
-    expected : Maybe String,
-    words: Array.Array String,
-    currentWordIndex: Int
-  }
+    { actual : String
+    , expected : String
+    , words : Array.Array String
+    , currentWordIndex : Int
+    }
 
 
-init : () -> (Model, Cmd Msg)
+init : () -> ( Model, Cmd Msg )
 init _ =
-  (loadWord { actual = "", expected = Nothing, words = Array.fromList ["foo", "bar", "baz"] , currentWordIndex = 0}, Cmd.none)
+    ( loadWord { actual = "", expected = "", words = Array.fromList [ "foo", "bar", "baz" ], currentWordIndex = 0 }, Cmd.none )
 
-loadWord: Model -> Model
+
+loadWord : Model -> Model
 loadWord model =
-  { model | expected = Array.get model.currentWordIndex model.words }
+    { model | expected = Maybe.withDefault "" (Array.get model.currentWordIndex model.words) }
+
+
+
+-- PORTS
+
+
+port say : String -> Cmd msg
+
 
 
 -- UPDATE
 
 
 type Msg
-  = KeyInput String
-  | ButtonNext
+    = KeyInput String
+    | ButtonNext
 
 
-update : Msg -> Model -> (Model, Cmd Msg)
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    KeyInput newContent ->
-      ({ model | actual = newContent }, Cmd.none)
-    ButtonNext ->
-      ( loadWord { model | currentWordIndex = model.currentWordIndex+1} , Cmd.none)
+    case msg of
+        KeyInput newContent ->
+            ( { model | actual = newContent }, Cmd.none )
+
+        ButtonNext ->
+            let
+                updated_model =
+                    loadWord { model | currentWordIndex = model.currentWordIndex + 1, actual = "" }
+            in
+            ( updated_model, say updated_model.expected )
+
 
 
 -- SUBSCRIPTIONS
@@ -55,7 +69,8 @@ update msg model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.none
+    Sub.none
+
 
 
 -- VIEW
@@ -63,25 +78,24 @@ subscriptions model =
 
 view : Model -> Document Msg
 view model =
-  {
-    title = "Spellings",
-    body = [
-  div []
-    [ input [ placeholder "Start typing!", value model.actual, onInput KeyInput, attribute "autofocus" ""] []
-    , viewValidation model ]
-    ]
-  }
+    { title = "Spellings"
+    , body =
+        [ div []
+            [ input [ placeholder "Start typing!", value model.actual, onInput KeyInput, attribute "autofocus" "" ] []
+            , viewValidation model
+            ]
+        ]
+    }
+
 
 
 --(button [ onClick ButtonNext ] [ text "-" ])
+
+
 viewValidation : Model -> Html Msg
 viewValidation model =
-  case model.expected of
-    Just expected ->
-      if model.actual == expected then
-        div [ style "color" "green" ] [ text "OK", button [ onClick ButtonNext ] [text "next"] ]
-      else
-        div [ style "color" "red" ] [ text "Keep trying!" ]
+    if model.actual == model.expected then
+        div [ style "color" "green" ] [ text "OK", button [ onClick ButtonNext ] [ text "next" ] ]
 
-    Nothing ->
-      text "Initialization error: missing words"
+    else
+        div [ style "color" "red" ] [ text "Keep trying!" ]
