58828507638f3f0e002f6a5624c313742aae0797
Neyts Zupan <nejc.zupan@gmail.com>
2023-11-04 19:25:55 +0000

    Preparing for v1

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 let
   nixpkgs = builtins.fetchTarball {
-    # https://status.nixos.org/ -> nixos-22.05 on 2022-10-23
-    url = "https://github.com/nixos/nixpkgs/archive/3933d8bb9120573c0d8d49dc5e890cb211681490.tar.gz";
+    # https://status.nixos.org/ -> nixos-23.05 on 2023-10-04
+    url = "https://github.com/nixos/nixpkgs/archive/621f51253edffa1d6f08d5fce4f08614c852d17e.tar.gz";
   };
   pkgs = import nixpkgs {};
 
@@ -10,7 +10,10 @@ in
 pkgs.mkShell {
   name = "dev-shell";
   buildInputs = [
-    pkgs.nodejs
+    pkgs.elmPackages.lamdera
+    pkgs.elmPackages.elm-review
+    pkgs.elmPackages.elm-test
+    pkgs.elmPackages.elm-format
   ];
 
 }
