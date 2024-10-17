1a2e77ec82583ecf27c0e54cd0dc1bde72e07e84
Neyts Zupan <zupo@users.noreply.github.com>
2024-10-17 13:49:34 +0100

    re-add back pre-commit-check

--- devenv.nix
+++ devenv.nix
@@ -1,15 +1,43 @@
 { pkgs, inputs, ... }:
 let
-  pkgs-unstable = import inputs.nixpkgs-unstable { system = pkgs.stdenv.system; };
+  pkgs-unstable = import inputs.nixpkgs-unstable { system = pkgs.stdenv.system; config.allowUnfree = true;  };
 in
 {
+
+  languages.elm.enable = true;
+
   packages = [
     pkgs.which
     pkgs.git
-    pkgs.elmPackages.lamdera
+    pkgs-unstable.elmPackages.lamdera
     pkgs-unstable.elmPackages.elm-test-rs
     pkgs.nodejs
   ];
+
+  pre-commit.hooks = {
+    actionlint.enable = true;
+    check-added-large-files.enable = true;
+    check-case-conflicts.enable = true;
+    check-executables-have-shebangs.enable = true;
+    check-merge-conflicts.enable = true;
+    check-shebang-scripts-are-executable.enable = true;
+    check-yaml.enable = true;
+    typos.enable = true;
+    deadnix.enable = true;
+    detect-private-keys.enable = true;
+    elm-format.enable = true;
+    elm-review.enable = true;
+    end-of-file-fixer.enable = true;
+    html-tidy.enable = true;
+    ripsecrets.enable = true;
+    trim-trailing-whitespace.enable = true;
+
+    shellcheck.enable = true;
+    shellcheck.excludes = [".yml" ".yaml"];
+
+    denofmt.enable = true;
+    denofmt.excludes = ["elm.json" "review/elm.json"];
+  };
 
   enterTest = ''
     lamdera make || true
