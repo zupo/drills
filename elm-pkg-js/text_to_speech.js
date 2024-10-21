0109ebd0a1a4e2766e5fa02544e64119626d4d9c
Neyts Zupan <zupo@users.noreply.github.com>
2024-10-21 12:54:44 +0100

    lamdera does something to semicolons?

--- elm-pkg-js/text_to_speech.js
+++ elm-pkg-js/text_to_speech.js
@@ -4,8 +4,8 @@ port text_to_speech : String -> Cmd msg
 
 exports.init = async function (app) {
   app.ports.text_to_speech.subscribe(function (message) {
-    utter = new SpeechSynthesisUtterance(message);
-    utter.rate = 0.7;
-    window.speechSynthesis.speak(utter);
-  });
-};
+    utter = new SpeechSynthesisUtterance(message)
+    utter.rate = 0.7
+    window.speechSynthesis.speak(utter)
+  })
+}
